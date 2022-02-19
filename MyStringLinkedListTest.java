35f86c4360b7e19e2f40744da4725ccc956164b3
musabmanan <99063115+musabmanan@users.noreply.github.com>
2022-02-18 23:19:57 -0600

    Add files via upload

---
+++ MyStringLinkedListTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class MyStringLinkedListTest {
+
+    @ParameterizedTest
+    @ValueSource( ints = {1,2,3,4,5})
+    void testRemove (int size) {
+        MyStringLinkedList objects  = new MyStringLinkedList();
+        for (int i =1; i <= size ; i++)
+            objects.add(new  String(String.valueOf(i)));
+    }
+
+
+    @ParameterizedTest
+    @ValueSource( ints = {1,2,3,4,5})
+    void testSize (int size) {
+        MyStringLinkedList objects  = new MyStringLinkedList();
+        for (int i =0; i <= size ; i++)
+            objects.add(new  String("bird"));
+        Assertions.assertEquals(size, objects.size());
+    }
+
+
+
+}
